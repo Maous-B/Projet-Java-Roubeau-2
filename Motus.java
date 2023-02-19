8a1df26464a467233417eb8dbf2ffb02cf36648d
Maous-B <79797065+Maous-B@users.noreply.github.com>
2023-02-19 17:16:07 +0100

    Create Motus.java

---
+++ Motus.java
@@ -0,0 +1,80 @@
+package proj;
+
+public class Motus {
+public static void main(String[] args) {
+		
+		
+		String motASaisir = "", X[], motATrouve;
+		int nbEssai, taille;
+		
+		motATrouve = Saisie.lire_String("Veuillez entrer un mot avec une taille supérieur à 5 : ");
+		
+		taille = motATrouve.length();
+		
+		while(taille < 5) 
+		{
+			motATrouve = Saisie.lire_String("Erreur de saisie. Veuillez recommencer : ");
+		}
+		
+		
+		X = new String[taille];
+		
+		
+		for(int I = 0; I < taille; I++) {
+			X[I] = motATrouve.substring(I, I+1);
+		}
+		
+		nbEssai = taille - 2;
+		
+		System.out.println("La taille du mot est de " + taille + ". Vous avez donc " + nbEssai + " essais.");
+		
+		
+		for(int A = 0; A < nbEssai; A++) {
+			motASaisir = Saisie.lire_String("Veuillez saisir votre essai avec la même taille que le mot a deviné. Si vous voulez abandonné, tapez SOL. Il vous reste " + (nbEssai-A) + " essais. : ");
+			
+			if(motASaisir.equals("SOL")) 
+			{
+				System.out.println("Vous avez tapé SOL. Vous avez abandonné et le mot était " + motATrouve);
+				A = 0;
+			}
+			else 
+			{
+				while(taille != motASaisir.length()) {
+					motASaisir = Saisie.lire_String("Erreur de saisie. Veuillez resaisir votre essai avec la même taille que le mot a deviné. Si vous voulez abandonné, tapez SOL. Il vous reste " + (nbEssai-A) + " essais. : ");
+				}
+				
+				if(motASaisir.equals(motATrouve)) 
+				{
+					System.out.println("BRAVO ! Vous avez trouvé le bon mot qu'était " + motATrouve + ".");
+					A = 0;
+				}
+				
+				else
+				{
+					System.out.print("Indice : ");
+					for(int I = 0; I < taille; I++) 
+					{
+						if(motASaisir.length() >= 5) {
+							if(X[I].equals(motASaisir.substring(I, I+1)) == true) 
+							{
+								System.out.print(X[I]);
+							}
+							else 
+							{
+								System.out.print("_");
+							}
+						}
+						
+					}
+					System.out.println(" ");
+				}
+			}
+		}
+		
+		
+	}
+
+}
+
+
+
